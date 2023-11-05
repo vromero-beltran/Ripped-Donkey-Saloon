c7c4399fc54fe5fe0274a982a1de50b8e6dd6aeb
Victor <frozen711.vr.vr@gmail.com>
2023-11-05 01:39:30 -0500

    added second calendar with JS

--- assets/upcoming.js
+++ assets/upcoming.js
@@ -20,7 +20,7 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
 }
 
 const updateDots = (currentDot, targetDot) => {
-    currentDot.clacclist.remove('current-slide');
+    currentDot.classlist.remove('current-slide');
     targetDot.classList.add('current-slide');
 }
 
@@ -79,6 +79,148 @@ dotsNav.addEventListener('click', e => {
 
     moveToSlide(track, currentSlide, targetSlide)
     updateDots(currentDot, targetDot);
-    hideShowArrows = (slides, prevButton, nextButton, targetIndex);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
 
 })
+
+
+// CALENDAR JAVASCRIPT
+const calendar = document.querySelector(".calendar");
+const date = document.querySelector(".date");
+const daysContainer = document.querySelector(".days");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const todayBtn = document.querySelector(".today-btn");
+const gotoBtn = document.querySelector(".goto-btn");
+const dateInput = document.querySelector(".date-input");
+
+let today = new Date();
+let activeDay;
+let month = today.getMonth();
+let year = today.getFullYear();
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+// fuction to add days
+function initCalendar() {
+    // to get prev month days and current month all days and remaining next month days
+    const firstDay = new Date(year, month, 1);
+    const lastDay = new Date(year, month + 1, 0);
+    const prevLastDay = new Date(year, month, 0);
+    const prevDays = prevLastDay.getDate();
+    const lastDate = lastDay.getDate();
+    const day = firstDay.getDay();
+    const nextDays = 7 - lastDay.getDay();
+
+    // updates the top of the calender
+    day.innerHTML = months[month] + " " + year; 
+
+    // adding days on DOM
+    let days = " ";
+
+    // prev month days
+// prev month days
+for (let x = day; x > 0; x--) {
+    days += `<div class="day prev-date">${prevDays - x + 1}</div>`;
+}
+
+
+    // current month days
+    for (let i = 1; i <= lastDate; i++){
+        // if say is today add class today
+        if (i === new Date().getDate() && year === new Date().getFullYear() && month  === new Date().getMonth()) {
+            days += `<div class="day today">${i}</div>`
+        }
+        // add remaining as it is
+        else {
+            days += `<div class="day" >${i}</div>`;
+        }
+    }
+    // next month days
+    for (let n = 1; n <= nextDays; n++) {
+            days += `<div class="day next-date">${n}</div>`;
+        }
+    daysContainer.innerHTML = days;
+
+}
+
+initCalendar();
+
+// prev month
+function prevMonth() {
+    month--;
+    if (month < 0){
+        month = 11;
+        year--;
+    }
+    initCalendar();
+}
+// next month
+function nextMonth() {
+    month++;
+    if(month > 11){
+        month = 0;
+        year++ ;
+    }
+    initCalendar();
+}
+
+// add eventlistiner on prev and next
+prev.addEventListener("click", prevMonth);
+next.addEventListener("click", nextMonth);
+
+// add goto date and goto today functionality
+todayBtn.addEventListener("click", () => {
+    today = new Date();
+    month = today.getMonth();
+    year = today.getFullYear();
+    initCalendar();
+});
+
+dateInput.addEventListener("keyup", (e) => {
+    // allows only numbers remove anything else
+    dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
+    if(dateInput.value.length === 2) {
+        // add a slash if 2 numbers entered
+        dateInput.value += "/";
+    }
+    if(dateInput.value.length > 7) {
+        dateInput.value = dateInput.value.slice(0, 7);
+    }
+    // if we remove unit slash its not removing
+    if(e.inputType === "deleteContentBackward") {
+        if(dateInput.value.length === 3) {
+            dateInput.value = dateInput.value.slice(0, 2);
+        }
+    }
+});
+
+// function to go to entered date
+
+function gotoDate() {
+    const dateArr = dateInput = dateInput.value.slit("/");
+    // some date validation
+    if (dateArr.length === 2) {
+        if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+            month = dateArr[0] - 1;
+            year = dateArr[1];
+            initCalendar();
+            return;
+        }
+    }
+    // if invalid date
+    alert('Invalid date');
+}
